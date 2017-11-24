8cc9fe0a05b98bf65ff1b19a937d4bf0a1608896
Berke Tezergil <btezergil@gmail.com>
2017-11-25 01:25:10 +0300

    Save method for EMController implemented, minor correction in Event

--- phase1/EMController.py
+++ phase1/EMController.py
@@ -13,6 +13,7 @@ class EMController:
                 cur = db.cursor()
             except Exception as e:
                 print("SQL Error while connecting", e)
+            
             try:
                 cur.execute("select * from MAP where ID='{}'".format(id))
                 mapfields = cur.fetchone()
@@ -20,17 +21,22 @@ class EMController:
                 print("SQL Error during selection of the map", e)
             self.eventmap.id = mapfields[0]
             self.eventmap.name = mapfields[1]
+            
             try:
                 cur.execute("select e.lon, e.lat, e.locname, e.title, e.desc, e.catlist, e.stime, e.ftime, e.timetoann from EVENT e where parentmap='{}'".format(id))
                 mapfields = cur.fetchall()
             except Exception as e:
                 print("SQL Error during selection of the events", e)
+            
             for e in mapfields:
                 newEvent = Event(e[0], e[1], e[2], e[3], e[4], e[5], e[6], e[7], e[8])
                 self.eventmap.insertEvent(newEvent, newEvent.lat, newEvent.lon)
+            db.close()
+
     def dettach(self):
         #dettach controller from map and clean all watches
         print("dettach called")
+    
     def save(self, name):
         ''' Saves currently attached EventMap object into the database '''
         # TODO: set up a database for this and then implement this method
@@ -43,9 +49,20 @@ class EMController:
         
         # try to get the map having the name 'name'
         try:
-            cur.execute("select * from MAP where NAME='{}'".format(name))
+            cur.execute("insert into map (id,name) values ({},'{}')".format(self.eventmap.id,name))
         except Exception as e:
-            print("SQL Error during insertion", e)
+            print("SQL Error during insertion of the map", e)
+
+        try:
+            for key,val in self.eventmap.events.items():
+                for ev in val:
+                    cur.execute('''insert into event (eid, lon, lat, locname, title, desc, catlist, stime, ftime, timetoann, parentmap) 
+                        values ({},{},{},'{}','{}','{}','{}','{}','{}','{}',{})'''.format(ev._id, ev.lon, ev.lat, ev.locname, ev.title, ev.desc, " ".join(str(item) for item in ev.catlist), ev.stime, ev.to, ev.timetoann, ev.parentmap.id))
+        except Exception as e:
+            print("SQL Error during insertion of the events", e)
+        db.commit()
+        db.close()
+    
     @classmethod
     def load(cls, name):
         ''' Loads the map saved as 'name' in the database and initializes an EventMap object with its attributes
@@ -71,9 +88,11 @@ class EMController:
         
         # before returning CLOSE THE DATABASE CONNECTION with db.close()
         db.close()
+    
     def list(self):
         ''' Lists all map objects stored in the database '''
         print("list called")
+    
     @classmethod
     def delete(cls, name):
         ''' Deletes the map object stored under name 'name' 
--- phase1/Event.py
+++ phase1/Event.py
@@ -17,12 +17,12 @@ class Event:
         self.stime = stime
         self.to = to
         self.timetoann = timetoann
-        self.parent_map = None
+        self.parentmap = None
         stv = datevalidator.match(stime)
         tv = datevalidator.match(to)
         ttav = datevalidator.match(timetoann)
-        #if stv == None or tv == None or ttav == None:
-            #raise ValueError("Date given in not accepted format or invalid date")
+        if stv == None or tv == None or ttav == None:
+            raise ValueError("Date given in not accepted format or invalid date")
         if not -90 < lat < 90:
             raise ValueError("Latitude not in range -90-90")
         if not -180 < lon < 180:
@@ -41,12 +41,12 @@ class Event:
         return self.__dict__
     def setMap(self, mapobj):
         ''' Attaches the event to the map object 'mapobj' '''
-        if self.parent_map != None:
+        if self.parentmap != None:
             print("event had a parent, performing cleanup from previous map")
             # event belonged to another map before. Delete the event from that map's tree and push it to the new map's tree.
             # TODO: implement deletion method to the tree and then call it from here
         mapobj.insertEvent(self, self.lat, self.lon)
-        self.parent_map = mapobj
+        self.parentmap = mapobj
     def getMap(self):
         ''' Returns the map that the event it attached to '''
-        return self.parent_map
+        return self.parentmap
