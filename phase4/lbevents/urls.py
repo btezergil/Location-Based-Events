d28632b13bddd5f21bd5f1e045a3f933d0c5a005
Berke Tezergil <btezergil@gmail.com>
2018-01-20 03:17:10 +0300

    Fixed urls.py after merge

--- phase4/lbevents/urls.py
+++ phase4/lbevents/urls.py
@@ -10,11 +10,8 @@ urlpatterns = [
 	path('listEvents/<int:mapid>', views.listEvents, name="Events of Map"),
 	path('detach/<int:mapid>', views.detach, name="Detach from Map"),
 	path('delete/<int:mapid>', views.deletemap, name="Delete Map"),
-<<<<<<< HEAD
 	path('addevent/<int:mapid>', views.createEvent, name="Create New Event"),
 	path('updevent/<int:eid>', views.createEvent, name="Update this Event"),
-=======
 	path('findclosest/<int:mapid>', views.findclosest, name="Find Closest"),
 	path('searchadvanced/<int:mapid>', views.searchadvanced, name="Search Advanced"),
->>>>>>> 611a32f92e244f1385d41c093cb7013b1d256e58
 ]
